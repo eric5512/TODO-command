8f4ca96299ef40876da940b692d4680461e4643a
Eric <ericdandria2@gmail.com>
2024-09-08 22:47:52 +0200

    Version 1.0 released

--- src/fileutil.rs
+++ src/fileutil.rs
@@ -1,10 +1,10 @@
 use std::fs;
-use std::io::Read;
+use std::io::{Result, Read};
 use std::path::Path;
 
-pub fn get_file_text(path: &Path) -> String {
+pub fn get_file_text(path: &Path) -> Result<String> {
     let mut file: fs::File = fs::File::open(path).expect("Unreacheable");
     let mut buf: String = String::from("");
-    file.read_to_string(&mut buf);
-    return buf;
+    file.read_to_string(&mut buf)?;
+    return Ok(buf);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::ffi::OsStr;
 use regex::Regex;
+use clap::Parser;
 
 mod regexlang;
 mod fileutil;
 
+
+/// Simple program to retrieve the TODOS in a project
+#[derive(Parser, Debug)]
+#[command(version="v1.0", about, long_about = None)]
+struct Args {
+    /// Look recursively into the nested folders
+    #[arg(short, long)]
+    recursive: bool,
+
+    /// Specify the extension of the files to look into
+    #[arg(short, long)]
+    extensions: Option<Vec<String>>
+}
+
 fn main() {
-    visit_files_rec("./");
+    let args: Args = Args::parse();
+    visit_files_rec("./", &args);
 }
 
 fn print_todos(path: &Path, regex: Regex) {
-    let text = fileutil::get_file_text(path);
+    let text: String = fileutil::get_file_text(path).expect(&format!("Warningn: Couldn't read the file {}", path.as_os_str().to_str().unwrap()));
 
-    let matches = regexlang::get_matches(&text, regex);
+    let matches: Vec<(u64, u64, &str)> = regexlang::get_matches(&text, regex);
 
     if matches.len() > 0 {
         print!("Found {} TODOS in file {}\n", matches.len(), path.display());
@@ -25,13 +41,20 @@ fn print_todos(path: &Path, regex: Regex) {
     }
 }
 
-fn visit_files_rec(path: &str) {
+fn visit_files_rec(path: &str, args: &Args) {
     for path in fs::read_dir(path).unwrap() {
-        let file_path = path.unwrap().path();
-        if let Some(regex) = file_path.extension().and_then(OsStr::to_str).and_then(regexlang::get_file_regex) {
-            print_todos(&file_path, regex);
-        } else if file_path.is_dir() {
-            visit_files_rec(file_path.to_str().unwrap());
+        let file_path: PathBuf = path.unwrap().path();
+        let extension: Option<&str> = file_path.extension().and_then(OsStr::to_str);
+        if let Some(regex) = extension.and_then(regexlang::get_file_regex) {
+            if let Some(exts) = &args.extensions {
+                if exts.contains(&extension.unwrap().to_string()) {
+                    print_todos(&file_path, regex);
+                }
+            } else {
+                print_todos(&file_path, regex);
+            }
+        } else if file_path.is_dir() && args.recursive {
+            visit_files_rec(file_path.to_str().unwrap(), args);
         }
     }
 }
